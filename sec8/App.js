4199e04c8f26555b1ff613e9935f26dff0987b8b
SOHTA0318 <nicosohta0318@gmail.com>
2022-01-08 23:21:18 -0800

    start sec 8

--- sec8/App.js
+++ sec8/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import ShopNavigator from "./navigation/ShopNavigator";
+
+import productsReducer from "./store/reducers/products";
+import { useState } from "react";
+
+const rootReducer = combineReducers({ products: productsReducer });
+
+const store = createStore(rootReducer);
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={(error) => {
+          console.log(error);
+        }}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ sec8/screens/user/UserProductsScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserProductsScreen = () => {
+  return <div></div>;
+};
+
+export default UserProductsScreen;
